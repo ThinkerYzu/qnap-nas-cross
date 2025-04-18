97a4e8d774960dc9b48bd43ccdd293a7d0e02cda
Kui-Feng Lee <thinker.li@gmail.com>
2025-04-18 16:40:37 -0700

    chsysroot.sh makes an executable using libraries installed in a directory.
    The build time libraries may not the same as the target system and we
    don't want to install our libraries to the system directories.  In
    order to install our library in an independent directory, we set rpath
    and interpreter for executsbles built by us.
    
    The script supposes that we copy our libraries to <sysroot>/lib and
    <sysroot>/usr/lib on the target system. It will add these two paths to
    rpath of the executable to make sure it uses libraries under these two
    directories. It also sets the interpreter path to the RTLD in
    <sysroot>/lib.

---
+++ scripts/chsysroot.sh
@@ -0,0 +1,23 @@
+# All required libraries are in the given sysroot path.
+
+# Check if patchelf is installed
+if ! command -v patchelf &> /dev/null; then
+    echo "patchelf could not be found. Please install it first."
+    exit 1
+fi
+
+help() {
+    echo "Usage: $0 <sysroot> <binary>"
+    echo "  sysroot: path to the sysroot directory of the target system"
+    echo "  binary: path to the binary to be patched"
+}
+if [ $# -ne 2 ]; then
+    help
+    exit 1
+fi
+
+SYSROOT=$1
+BINARY=$2
+patchelf --set-rpath $SYSROOT/lib $BINARY
+patchelf --add-rpath $SYSROOT/usr/lib $BINARY
+patchelf --set-interpreter $SYSROOT/lib/ld-linux-aarch64.so.1 $BINARY

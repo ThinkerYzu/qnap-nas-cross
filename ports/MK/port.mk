2f73aaa6c74a80d31bb8d87697daa0b00a3d4726
Kui-Feng Lee <thinker.li@gmail.com>
2025-04-20 10:33:53 -0700

    ports: run chrootsys.sh on all binaries listed in the BINS var.
    The caller has to provide DEST_SYSROOT that is the path that binaries
    and libraries will be installed on the target machine. The path is
    where toolchain/ will be on the target machine. For example,
    toolchain/ will be /path/to/myroot/ on the target machine, then
    DEST_SYSROOT should be /path/to/myroot.
    
    For example, in a port directory:
    
        make DEST_SYSROOT=/path/to/myroot initsysroot
    
    Or, you defined DEST_SYSROOT in dest-config.mk at the source root
    directory of this build system.

--- ports/MK/port.mk
+++ ports/MK/port.mk
@@ -1,7 +1,23 @@
+-include $(ROOT)/dest-config.mk
+
+ROOT ?= $(realpath $(CURDIR)/../..)
+TOOLCHAIN ?= $(ROOT)/toolchain
+
 pkg:
 	@echo "Packaging $(PKG_NAME)"
 	@mkdir -p dist
 	@cd $(TOOLCHAIN) && \
 		tar zcvfT $(CURDIR)/dist/$(PKG_NAME).tgz $(CURDIR)/filelist
 	@echo "Package $(PKG_NAME) created at $(CURDIR)/dist/$(PKG_NAME).tgz"
+
+initsysroot:
+	@echo "Init sysroot for binaries"
+	@mkdir -p $(TOOLCHAIN)
+	@test -n "$(DEST_SYSROOT)" || \
+		{ echo "Please provide 'DEST_SYSROOT' first"; exit 1; }
+	@cd $(TOOLCHAIN) && \
+		for i in $(BINS); do \
+			echo "Including $${i}"; \
+			$(ROOT)/scripts/chsysroot.sh $(DEST_SYSROOT) $${i}; \
+		done
 
